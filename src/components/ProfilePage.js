2905a2e839170d11d86e3a2522367bf9444ca4ae
wolf-dominion <hannah_drury@yahoo.com>
2020-06-22 18:12:56 -0600

    left comments for when I get back

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -4,12 +4,16 @@ import ListofResults from './ListofResults'
 
 class ProfilePage extends Component{
 
+    getProgress = () => {
+        // code to gather overall progress score. average of each of the 3 components, then average of those
+    }
 
     render(){
         
         return(
             <div>
                 <h1>Profile page</h1>
+                <h2>Overall Progress: {}</h2>
                 <ListofResults results={this.props.results}/>
             </div>
         )
