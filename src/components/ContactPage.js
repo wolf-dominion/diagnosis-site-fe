815d503dea9eb6e01d75baced015c69b66f5d85e
wolf-dominion <hannah_drury@yahoo.com>
2020-07-07 15:59:57 -0600

    center and make contact form smaller

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -5,48 +5,48 @@ import axios from 'axios';
 
 class ContactPage extends Component{
 
-    state = {
-        name: '',
-        message: '',
-        email: '',
-        sent: false,
-        buttonText: 'Send Message'
-    }
+    // state = {
+    //     name: '',
+    //     message: '',
+    //     email: '',
+    //     sent: false,
+    //     buttonText: 'Send Message'
+    // }
 
-    formSubmit = (e) => {
-        e.preventDefault()
+    // formSubmit = (e) => {
+    //     e.preventDefault()
       
-        this.setState({
-            buttonText: '...sending'
-        })
+    //     this.setState({
+    //         buttonText: '...sending'
+    //     })
       
-        let data = {
-            name: this.state.name,
-            email: this.state.email,
-            message: this.state.message
-        }
+    //     let data = {
+    //         name: this.state.name,
+    //         email: this.state.email,
+    //         message: this.state.message
+    //     }
         
-        axios.post('API_URI', data)
-        .then( res => {
-            this.setState({ sent: true }, this.resetForm())
-        })
-        .catch( () => {
-          console.log('Message not sent')
-        })
-      }
+    //     axios.post('API_URI', data)
+    //     .then( res => {
+    //         this.setState({ sent: true }, this.resetForm())
+    //     })
+    //     .catch( () => {
+    //       console.log('Message not sent')
+    //     })
+    //   }
 
-      resetForm = () => {
-        this.setState({
-            name: '',
-            message: '',
-            email: '',
-            buttonText: 'Message Sent'
-        })
-    }
+    //   resetForm = () => {
+    //     this.setState({
+    //         name: '',
+    //         message: '',
+    //         email: '',
+    //         buttonText: 'Message Sent'
+    //     })
+    // }
 
     render(){
         return(
-            <div>
+            <div className="form-holder">
                 <div className="contact-form-container">
                    <ContactForm />
                 </div>
